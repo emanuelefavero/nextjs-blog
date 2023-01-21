374debcd16e73d118f54e75fad90eb131a577fc0
emanuelefavero <emafaux89@gmail.com>
2023-01-21 23:49:29 +0100

    add vercel host url and re-enable hello.js

--- config/server.js
+++ config/server.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
+export const server = dev
+  ? 'http://localhost:3000'
+  : 'https://nextjs-blog-g2nx.vercel.app'
--- pages/hello.js
+++ pages/hello.js
@@ -1,21 +1,22 @@
-export default function hello() {
-  return <h1 className='text-indigo-500'>Hello</h1>
+const { server } = require('@/config/server')
+
+export default function hello({ data }) {
+  return <h1 className='text-indigo-500'>{data.text}</h1>
 }
 
-// const { server } = require('@/config/server')
+// ? if data is changing often, we should use getServerSideProps
+export async function getStaticProps() {
+  const res = await fetch(`${server}/api/hello`)
+  const data = await res.json()
 
-// export default function hello({ data }) {
-//   return <h1 className='text-indigo-500'>{data.text}</h1>
-// }
+  return {
+    props: {
+      data,
+    },
+  }
+}
 
-// // ? if data is changing often, we should use getServerSideProps
-// export async function getStaticProps() {
-//   const res = await fetch(`${server}/api/hello`)
-//   const data = await res.json()
-
-//   return {
-//     props: {
-//       data,
-//     },
-//   }
+// ? use this snippet to test app without api data
+// export default function hello() {
+//   return <h1 className='text-indigo-500'>Hello</h1>
 // }
