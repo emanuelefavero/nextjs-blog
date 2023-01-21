cbecd419c19d4d3160fbd9c208261885b3809ac5
emanuelefavero <emafaux89@gmail.com>
2023-01-21 23:44:12 +0100

    comment out hello.js to find out assigned vercel url

--- pages/hello.js
+++ pages/hello.js
@@ -1,14 +1,21 @@
-export default function hello({ data }) {
-  return <h1 className='text-indigo-500'>{data.text}</h1>
+export default function hello() {
+  return <h1 className='text-indigo-500'>Hello</h1>
 }
 
-export async function getStaticProps() {
-  const res = await fetch('http://localhost:3000/api/hello')
-  const data = await res.json()
+// const { server } = require('@/config/server')
 
-  return {
-    props: {
-      data,
-    },
-  }
-}
+// export default function hello({ data }) {
+//   return <h1 className='text-indigo-500'>{data.text}</h1>
+// }
+
+// // ? if data is changing often, we should use getServerSideProps
+// export async function getStaticProps() {
+//   const res = await fetch(`${server}/api/hello`)
+//   const data = await res.json()
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   }
+// }
